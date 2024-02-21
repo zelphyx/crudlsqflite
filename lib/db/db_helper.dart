60d3479829db200fc05b7c1a0549cbac4456d64c
zelphyx <rizqy.astiko19@gmail.com>
2024-02-21 16:44:25 +0700

    first commit

--- lib/db/db_helper.dart
+++ lib/db/db_helper.dart
@@ -1,13 +1,15 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseHelper {
   static const String _dbPath = "notes_db.db";
-  static late String _dbDir;
   static Database? _db;
 
   static Future<void> init() async {
-    _dbDir = await getDatabasesPath();
-    _db = await openDatabase("$_dbDir/$_dbPath",
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    _db = await openDatabase("$documentDirectory.path/$_dbPath",
         onCreate: (Database db, int version) async {
           await db.execute("""
         CREATE TABLE Notes (
